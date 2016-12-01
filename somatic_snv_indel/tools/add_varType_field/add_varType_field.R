93758593f66002cbc166267fa1192e00d6227e7c
davidroberson <david.roberson@gmail.com>
2016-12-01 18:58:34 +0000

    adding "add var type" tool

---
+++ somatic_snv_indel/tools/add_varType_field/add_varType_field.R
@@ -0,0 +1,13 @@
+require(tidyverse); require(jsonlite)
+
+output = file(args_vcf_json_out, open = "wb")
+json_df = stream_in(file(args_vcf_json_in$path), pagesize=1,
+handler= function(df){
+
+temp = df
+
+temp$var_type = args_var_type
+
+stream_out(temp, output)})
+close(output)
+
